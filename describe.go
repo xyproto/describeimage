3736b3b2f057a72dbb378a24d0bc990337f0244d
Alexander F. Rødseth <rodseth@gmail.com>
2024-06-21 12:11:33 +0200

    Refactor + improve the default prompt

---
+++ describe.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/xyproto/ollamaclient/v2"
+	"github.com/xyproto/wordwrap"
+)
+
+// describeImages uses Ollama and the given model to describe one or more images.
+// prompt is the start of the multimodal prompt: the instructions which will be followed by one or more images
+// outputFile is the file to write the result to (can be blank to not write to anything)
+// model is the model to use, like llava
+// writeWidth is the width that the returned or written string should be wrapped to, if it is >0
+// filenames is a list of input images
+// A description is returned as a string.
+func describeImages(prompt, outputFile, model string, wrapWidth int, filenames []string) (string, error) {
+	if wrapWidth == -1 {
+		wrapWidth = getTerminalWidth()
+	}
+
+	if len(filenames) < 1 {
+		return "", fmt.Errorf("no image filenames provided")
+	}
+
+	var images []string
+	for _, filename := range filenames {
+		logVerbose("[%s] Reading... ", filename)
+		base64image, err := ollamaclient.Base64EncodeFile(filename)
+		if err == nil { // success
+			images = append(images, base64image)
+			logVerbose("OK\n")
+		} else {
+			logVerbose("FAILED: " + err.Error() + "\n")
+		}
+	}
+
+	if len(images) == 0 {
+		return "", fmt.Errorf("no images to describe")
+	}
+
+	if prompt == "" {
+		if len(images) > 1 {
+			prompt = "Describe the following images:"
+		} else {
+			prompt = "Describe the this image:"
+		}
+	}
+
+	oc := ollamaclient.New()
+	oc.ModelName = model
+
+	if err := oc.PullIfNeeded(verbose); err != nil {
+		return "", fmt.Errorf("error: %v", err)
+	}
+	oc.SetReproducible()
+
+	promptAndImages := append([]string{prompt}, images...)
+
+	logVerbose("[%s] Generating... ", oc.ModelName)
+	output, err := oc.GetOutput(promptAndImages...)
+	if err != nil {
+		return "", fmt.Errorf("error: %v", err)
+	}
+	logVerbose("OK\n")
+
+	if output == "" {
+		return "", fmt.Errorf("generated output for the prompt %s is empty", prompt)
+	}
+
+	if wrapWidth > 0 {
+		lines, err := wordwrap.WordWrap(output, wrapWidth)
+		if err == nil { // success
+			output = strings.Join(lines, "\n")
+		}
+	}
+
+	if outputFile != "" {
+		err := os.WriteFile(outputFile, []byte(output), 0o644)
+		if err != nil {
+			return "", fmt.Errorf("error writing to file: %v", err)
+		}
+		return "", nil
+	}
+
+	return output, nil
+}
--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/pflag"
-	"github.com/xyproto/ollamaclient/v2"
-	"github.com/xyproto/wordwrap"
 	"golang.org/x/term"
 )
 
@@ -34,79 +31,6 @@ func getTerminalWidth() int {
 	return width
 }
 
-func describeImages(promptHeader, outputFile, model string, wrapWidth int, filenames []string) (string, error) {
-	if wrapWidth == -1 {
-		wrapWidth = getTerminalWidth()
-	}
-
-	if len(filenames) < 1 {
-		return "", fmt.Errorf("no image filenames provided")
-	}
-
-	var images []string
-	for _, filename := range filenames {
-		logVerbose("[%s] Reading... ", filename)
-		base64image, err := ollamaclient.Base64EncodeFile(filename)
-		if err == nil { // success
-			images = append(images, base64image)
-			logVerbose("OK\n")
-		} else {
-			logVerbose("FAILED: " + err.Error() + "\n")
-		}
-	}
-
-	var prompt string
-	switch len(images) {
-	case 0:
-		return "", fmt.Errorf("no images to describe")
-	case 1:
-		prompt = "Describe this image:"
-	default:
-		prompt = "Describe these images:"
-	}
-	if promptHeader != "" {
-		prompt = promptHeader
-	}
-
-	oc := ollamaclient.New()
-	oc.ModelName = model
-
-	if err := oc.PullIfNeeded(verbose); err != nil {
-		return "", fmt.Errorf("error: %v", err)
-	}
-	oc.SetReproducible()
-
-	promptAndImages := append([]string{prompt}, images...)
-
-	logVerbose("[%s] Generating... ", oc.ModelName)
-	output, err := oc.GetOutput(promptAndImages...)
-	if err != nil {
-		return "", fmt.Errorf("error: %v", err)
-	}
-	logVerbose("OK\n")
-
-	if output == "" {
-		return "", fmt.Errorf("generated output for the prompt %s is empty", prompt)
-	}
-
-	if wrapWidth > 0 {
-		lines, err := wordwrap.WordWrap(output, wrapWidth)
-		if err == nil { // success
-			output = strings.Join(lines, "\n")
-		}
-	}
-
-	if outputFile != "" {
-		err := os.WriteFile(outputFile, []byte(output), 0o644)
-		if err != nil {
-			return "", fmt.Errorf("error writing to file: %v", err)
-		}
-		return "", nil
-	}
-
-	return output, nil
-}
-
 func main() {
 	var (
 		promptHeader, outputFile, model string
@@ -115,7 +39,7 @@ func main() {
 	)
 
 	pflag.BoolVarP(&verbose, "verbose", "V", false, "verbose output")
-	pflag.StringVarP(&promptHeader, "prompt", "p", "Write a short summary of what a project that contains the following files is:", "Provide a custom prompt header")
+	pflag.StringVarP(&promptHeader, "prompt", "p", "Describe the following image(s):", "Provide a custom prompt header")
 	pflag.StringVarP(&outputFile, "output", "o", "", "Specify an output file")
 	pflag.StringVarP(&model, "model", "m", defaultModel, "Specify the Ollama model to use")
 	pflag.IntVarP(&wrapWidth, "wrap", "w", 0, "Word wrap at specified width. Use '-1' for terminal width")
