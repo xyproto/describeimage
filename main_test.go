4f213e421157a72a4ed057d3e8b1058219d834bc
Alexander F. Rødseth <rodseth@gmail.com>
2026-06-04 16:32:31 +0200

    Update tests

--- main_test.go
+++ main_test.go
@@ -13,17 +13,17 @@ const (
 	verbose      = true
 )
 
-func TestImageDescriptionContainsPuppy(t *testing.T) {
+func TestImageDescriptionContainsHorses(t *testing.T) {
 	// Define the input parameters
-	filenames := []string{"img/puppy.png"}
+	filenames := []string{"img/horses.png"}
 	// Call the describeImages function
 	output, err := describeImages("", "", "", wrapWidth, filenames, verbose)
 	if err != nil {
 		t.Fatalf("describeImages failed: %v", err)
 	}
-	// Check if the output contains the word "puppy"
-	if !strings.Contains(output, "puppy") {
-		t.Errorf("Expected output to contain 'puppy', but it did not. Output: %s", output)
+	// Check if the output contains the word "horses"
+	if !strings.Contains(output, "horses") {
+		t.Errorf("Expected output to contain 'horses', but it did not. Output: %s", output)
 	}
 }
 
